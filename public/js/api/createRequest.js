4d591015178e6f156151e123b3afd9851fcefcc3
Ekaterina L <Ekaterin.Lys@yandex.ru>
2023-05-07 04:30:42 +0300

    first commit: createRequest was changed

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,33 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+    let {url, data, method, callback} = options;
 
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    let formData = null;
+
+    xhr.addEventListener("load", () => {
+        if ( xhr.readyState === 4 && xhr.status === 200 ) {
+            callback(null, xhr.response);
+        } else {
+            callback(err);
+        }
+    });
+
+    if ( method === "GET") {
+        url += "?"
+        for ( let pos in data ) {
+            url += `${pos}=${data[pos]}&`;
+        }
+        xhr.open(method, url.slice(0, -1));
+    } else {
+        formData = new FormData();
+        for ( let pos in data ) {
+            formData.append(`${pos}`, `${data[pos]}`);  // ???
+        }
+        xhr.open(method, url);
+    }
+
+    xhr.send(formData);
 };
